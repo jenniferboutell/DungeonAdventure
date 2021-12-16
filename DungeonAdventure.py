3f25ce9f28c3d68eb5ebb822e35d37a8f945c35a
jenniferboutell <57915509+jenniferboutell@users.noreply.github.com>
2021-12-15 20:22:58 -0800

    Finished draft basic game play. Still needs pillars.

--- DungeonAdventure.py
+++ DungeonAdventure.py
@@ -35,7 +35,7 @@ class DungeonAdventure:
 
     def start(self):
         start_option = input("Shall we play a game? (Y/N)\n")
-        if start_option == "no":
+        if start_option == "N":
             print("Commence global thermonuclear war in 3... 2... 1...")
             print(".....haha, not really. That is scheduled for tomorrow.")
 
@@ -57,6 +57,7 @@ class DungeonAdventure:
         print("Here are your options...\n",
               "Move (N)orth, (S)outh, (E)ast, or (W)est;\n",
               "Use (V)ision Potion or (H)ealing Potion;\n"
+              "Display (I)nventory;\n"
               "Show (M)ap; (Q)uit the game; or (?) to get help")
         # Do NOT reveal the hidden '@' option to show full map
 
@@ -97,17 +98,22 @@ class DungeonAdventure:
         elif option == 'V':
             print("You guzzle down the crystal clear fluid.")
             self.__hero.use_vision_potion()
+            #Todo: finish ues vision potion method in Adventurer
 
         elif option == "Joshua":
             print("A strange game. The only winning move is not to play.")
             # no-op
 
+        elif option =="I":
+            self.__hero.display_inventory()
+
         elif option in ('N', 'S', 'E', 'W'):
-            print(f"You take a step to the {option}...")
             next_room = self.__room.neighbor(option)
             if next_room is None:
                 print("There is no room in this direction.")
+                print(f"And lo, for Brave, Brave, Brave Sir {self.name} did stand still.")
                 return
+            print(f"You take a step to the {option}...")
             self.__room = next_room
             # self.__room.set_room()  # FIXME nope, rely on loaded map!
             print(self.__room)
@@ -117,6 +123,10 @@ class DungeonAdventure:
                 self.find_vision_potion()
             if self.__room.has_pit:
                 self.fall_into_pit()
+            if self.__hero.hit_points <= 0:
+                self.__continues = False
+            if self.__room.is_exit:
+                self.find_exit()  #Todo: add pillar logic
         else:
             print("These words that you are using... I do not think they mean\n",
                   "what you think they mean. Please try again.")
@@ -130,13 +140,22 @@ class DungeonAdventure:
 
     def find_healing_potion(self):
         print("You find a healing potion. Use this to restore some lost hit-points.")
+        self.__hero.gain_healing_potion()
 
     def find_vision_potion(self):
         print("You find a vision potion. Use this to see surrounding rooms.")
+        self.__hero.gain_vision_potion()
 
     def fall_into_pit(self):
         print("You fall into a pit. The fall is merely frightening; the landing hurts.")
+        self.__hero.take_damage()
+        print("You now have " + str(self.__hero.hit_points) + " hit points. Ouch.")
+        #To DO: fix adventurer so that damage is randomly set, and hit points statement set there as well.
 
+    def find_exit(self):
+        #todo: add this logic: if current room is exit
+            print("Dilly Dilly! Brave Brave Brave Brave Sir " + self.name + " has found the exit.")
+            self.__continues = False
 
 class MockAdventurer:
 
